c158a18a410f003a1f2c13e5b3b630499cae7734
elane <chagaram0610@gmail.com>
2023-11-03 10:07:16 +0900

    design : button 공통 style

---
+++ src/components/CommonButton.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ConmmonButton = styled.button`
+  font-family: "S-CoreDream-medium", sans-serif;
+
+  outline: none;
+  border: none;
+  border-radius: 8px;
+
+  padding-top: 8px;
+  padding-bottom: 8px;
+  width: 100%;
+
+  cursor: pointer;
+`;
