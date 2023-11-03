576473ed8f8a1b076b5e2fe2c0e29ca91f124e34
elane <chagaram0610@gmail.com>
2023-11-03 09:50:13 +0900

    design : Home 디자인

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import MainImage from "./assets/MainBackgroundImage.png";
 import Payment from "./pages/payment";
+import Home from "./pages/home";
 import BranchReservation from "./pages/branchReservation";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <img src={MainImage} alt="MainImage" /> */}
+      <Home />
       <BrowserRouter>
         <Routes>
           <Route path="/"></Route>
