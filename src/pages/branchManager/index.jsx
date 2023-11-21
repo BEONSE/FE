10d61759c3224a3d01e1a97082d163eec1396113
elane <chagaram0610@gmail.com>
2023-11-21 13:06:47 +0900

    feat : 지점사 Home Route

--- src/pages/branchManager/index.jsx
+++ src/pages/branchManager/index.jsx
@@ -1,7 +1,46 @@
+import { Routes, Route } from "react-router-dom";
+import BranchReview from "./review";
+import CouponList from "./coupon";
+import BranchUpdate from "./branchUpdate";
+import { usePageMoving } from "../../components/usePageMoving";
+
 const BranchManager = () => {
+  const { moveToMyBranchReview, moveToBranchUpdate, moveToUseCoupons } = usePageMoving();
   return (
     <>
       <h1>지점사 관리 페이지</h1>
+      <Routes>
+        <Route path="/review" element={<BranchReview />} />
+        <Route path="/coupons" element={<CouponList />} />
+        <Route path="/mypage" element={<BranchUpdate />} />
+      </Routes>
+      <div>
+        <p
+          onClick={() => {
+            moveToMyBranchReview();
+          }}
+        >
+          리뷰
+        </p>
+      </div>
+      <div>
+        <p
+          onClick={() => {
+            moveToUseCoupons();
+          }}
+        >
+          쿠폰
+        </p>
+      </div>
+      <div>
+        <p
+          onClick={() => {
+            moveToBranchUpdate();
+          }}
+        >
+          정보 수정
+        </p>
+      </div>
     </>
   );
 };
