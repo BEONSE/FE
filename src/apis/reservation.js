71ca7451ab0b0c12ba07558f7eeeeded0434e1bb
elane <chagaram0610@gmail.com>
2023-11-23 18:02:43 +0900

    feat : 세차장 예약 기능 추가

--- src/apis/reservation.js
+++ src/apis/reservation.js
@@ -1,8 +1,21 @@
 import { axios1 as axios } from "./axiosConfig";
 
 // 지점 예약 상황 불러오기
+export function ReqReservationState(bid, date) {
+  return axios.get(`/reservation/${bid}/${date}`);
+}
+
+// 예약할 지점명 가져오기
+export function ReqBranchName(bid) {
+  return axios.get(`/reservation/${bid}`);
+}
 
 // 지점 예약하기
 export function ReqBranchReserve(bid, reservationInfo) {
   return axios.post(`/branches/${bid}/reservation`, reservationInfo);
+}
+
+// 내 예약 목록 가져오기
+export function ReqMyReservation() {
+  return axios.get(`/mypage/reservation`);
 }
--- src/pages/myPages/myReservation/index.jsx
+++ src/pages/myPages/myReservation/index.jsx
@@ -1,13 +1,34 @@
 import styled from "styled-components";
 import BackMove from "../../../components/backMove";
 import React from "react";
+import { useEffect } from "react";
+import { ReqMyReservation } from "../../../apis/reservation";
+import { useState } from "react";
+import ReservationItem from "./ReservationItem";
 
 const MyReservation = () => {
+  const [resList, setResList] = useState([]);
+
+  useEffect(() => {
+    async function getReservation() {
+      try {
+        const reserveResponse = await ReqMyReservation();
+        console.log(reserveResponse);
+        if (reserveResponse.status === 200) {
+          setResList(reserveResponse.data.content);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getReservation();
+  }, []);
+
   return (
     <>
       <BackMove />
       <Title>예약 현황</Title>
-    
+      {resList && resList.map((list) => <ReservationItem key={list.rvid} list={list} />)}
     </>
   );
 };
