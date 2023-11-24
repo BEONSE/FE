d3c76a0774f2f8ac43662c5b061cda5a00a62bff
elane <chagaram0610@gmail.com>
2023-11-24 20:26:45 +0900

    design : 예약 확인 css 수정

--- src/pages/myPages/myReservation/ReservationItem.jsx
+++ src/pages/myPages/myReservation/ReservationItem.jsx
@@ -1,25 +1,82 @@
 import styled from "styled-components";
 import { CommonButton } from "../../../components/CommonButton";
+import { useEffect, useState } from "react";
 
 const ReservationItem = ({ list }) => {
+  // 문자열을 Date 객체로 변환
+  const reservationTimeDate = new Date(list.reservationTime);
+
+  // 연도, 월, 일 추출
+  const year = reservationTimeDate.getFullYear();
+  const month = reservationTimeDate.getMonth(); // getMonth()는 0부터 시작하므로 +1 해줍니다.
+  const day = reservationTimeDate.getDate();
+
+  // 목표 날짜 설정 (YYYY, MM - 1, DD)
+  const targetDate = new Date(year, month, day); // 2023년 11월 25일을 목표 날짜로 설정
+
+  // 현재 날짜 가져오기
+  const currentDate = new Date();
+
+  // 디데이 계산
+  const diffInTime = targetDate.getTime() - currentDate.getTime();
+  const diffInDays = Math.floor(diffInTime / (1000 * 3600 * 24));
+
+  const [timeOver, setTimeOver] = useState(false);
+
+  useEffect(() => {
+    setTimeOver(diffInDays <= 0);
+  }, [diffInDays]);
+
   return (
     <>
-      <ReseveAllDiv>
+      <ReserveAllDiv>
         <BranchName>
-          <p>{list.branchName}</p>
+          <p>BEONSE {list.branchName}</p>
         </BranchName>
         <ResTime>
           <p>{list.reservationTime}</p>
+          <DDayBox finish={diffInDays >= 0}>
+            {diffInDays > 0 ? <h3>D-{diffInDays}</h3> : <h3>예약 만료</h3>}
+          </DDayBox>
         </ResTime>
-        <CancleBtn>예약 취소</CancleBtn>
-      </ReseveAllDiv>
+        <CancelButton over={timeOver}>예약 취소</CancelButton>
+      </ReserveAllDiv>
     </>
   );
 };
 
 export default ReservationItem;
 
-const ReseveAllDiv = styled.div``;
-const BranchName = styled.div``;
-const ResTime = styled.div``;
-const CancleBtn = styled(CommonButton)``;
+const ReserveAllDiv = styled.div`
+  width: 90vw;
+  box-shadow: 0px 0px 6px rgba(0, 0, 0, 0.2);
+  margin: auto;
+  border-radius: 20px;
+  margin-bottom: 3vh;
+  padding: 5%;
+`;
+
+const BranchName = styled.div`
+  & > p {
+    font-size: 20px;
+    font-weight: bold;
+  }
+`;
+
+const ResTime = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const CancelButton = styled(CommonButton)`
+  margin-top: 2vh;
+  pointer-events: ${(props) => (props.over ? "none" : "auto")};
+  background-color: ${({ over }) => (over ? "#ececec" : "")};
+`;
+
+const DDayBox = styled.div`
+  & > h3 {
+    font-size: 20px;
+    color: ${(props) => (props.finish ? "red" : "#cccccc")};
+  }
+`;
